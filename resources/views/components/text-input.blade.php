7995035cdbca74fc233c7ae7729abcf854c2c4ed
MazharSolkar <mazharsolkar65@gmail.com>
2024-09-05 18:05:02 +0530

    Refactor: Plain Js to Alpine.js (clear input button of filter)

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -1,8 +1,7 @@
 <div class="relative">
-    @if ($formId)    
+    @if ($formRef)    
         <button type="button"
-        onclick="document.getElementById('{{$name}}').value ='';
-        document.getElementById('{{$formId}}').submit();"
+        @click="$refs['input-{{$name}}'].value = ''; $refs['{{$formRef}}'].submit();"
         class="absolute top-0 right-0 flex h-full items-center pr-2" 
         >
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5 text-slate-500">
@@ -10,5 +9,5 @@
             </svg>
         </button>
     @endif
-    <input type="text" name="{{$name}}" value="{{$value}}" id="{{$name}}" placeholder="{{$placeholder}}" class="w-full pr-8 rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2">
+    <input x-ref="input-{{$name}}" type="text" name="{{$name}}" value="{{$value}}" id="{{$name}}" placeholder="{{$placeholder}}" class="w-full pr-8 rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2">
 </div>
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -5,19 +5,19 @@
     :links="['Jobs'=> route('jobs.index')]"
     />
 
-    <x-card class="mb-4 text-sm">
-        <form id="filtering-form" action="{{route('jobs.index')}}" method="GET">
+    <x-card x-data="" class="mb-4 text-sm">
+        <form x-ref="filters" id="filtering-form" action="{{route('jobs.index')}}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input form-id="filtering-form" name="search" value="{{request('search')}}" placeholder="Search for any text" />
+                    <x-text-input form-ref="filters" name="search" value="{{request('search')}}" placeholder="Search for any text" />
                 </div>
 
                 <div>
                     <div class="mb-1 font-semibold">Salary</div>
                     <div class="flex space-x-2">
-                        <x-text-input form-id="filtering-form" name="min_salary" value="{{request('min_salary')}}" placeholder="From" />
-                        <x-text-input form-id="filtering-form" name="max_salary" value="{{request('max_salary')}}" placeholder="to" />
+                        <x-text-input form-ref="filters" name="min_salary" value="{{request('min_salary')}}" placeholder="From" />
+                        <x-text-input form-ref="filters" name="max_salary" value="{{request('max_salary')}}" placeholder="to" />
                     </div>
                 </div>
 
