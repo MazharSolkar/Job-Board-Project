85e868f79140b6f9e6dbc982cccb836f16947735
MazharSolkar <mazharsolkar65@gmail.com>
2024-09-05 16:24:34 +0530

    Filtering Jobs: Configuring Lables and Array in PHP

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -7,7 +7,7 @@
     @foreach ($options as $option)
         <label for="{{$name}}" class="mb-1 flex items-center">
             <input type="radio" name="{{$name}}" value="{{$option}}" @checked(request($name) === $option) />
-            <span class="ml-2">{{$option}}</span>
+            <span class="ml-2">{{ucfirst($option)}}</span>
         </label>
     @endforeach
 </div>
