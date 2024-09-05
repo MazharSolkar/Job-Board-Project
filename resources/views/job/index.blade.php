db1d8ce77e818454a208a169af68e2af6b951bea
MazharSolkar <mazharsolkar65@gmail.com>
2024-09-05 16:57:56 +0530

    Filtering Jobs: Clearing the Input

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -6,11 +6,11 @@
     />
 
     <x-card class="mb-4 text-sm">
-        <form action="{{route('jobs.index')}}" method="GET">
+        <form id="filtering-form" action="{{route('jobs.index')}}" method="GET">
             <div class="mb-4 grid grid-cols-2 gap-4">
                 <div>
                     <div class="mb-1 font-semibold">Search</div>
-                    <x-text-input name="search" value="{{request('search')}}" placeholder="Search for any text" />
+                    <x-text-input form-id="filtering-form" name="search" value="{{request('search')}}" placeholder="Search for any text" />
                 </div>
 
                 <div>
