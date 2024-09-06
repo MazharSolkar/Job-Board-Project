749094c6ed5274049b97ad56f4d036d2a321d89a
MazharSolkar <mazharsolkar65@gmail.com>
2024-09-06 16:08:55 +0530

    Employer: Model, Migration, Relation

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BeglongsTo;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
@@ -12,12 +13,12 @@ class Job extends Model
     use HasFactory;
 
     public static array $experience = ['entry', 'intermediate', 'senior'];
-    public static array $category = [
-        'IT',
-        'Finance',
-        'Sales',
-        'Marketing'
-    ];
+    public static array $category = ['IT', 'Finance', 'Sales', 'Marketing'];
+
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
 
     public function scopeFilter(Builder | QueryBuilder $query, array $filters): Builder | QueryBuilder 
     {
--- app/Models/User.php
+++ app/Models/User.php
@@ -16,21 +16,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name', 'email', 'password',];
 
     /**
      * The attributes that should be hidden for serialization.
      *
      * @var array<int, string>
      */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected $hidden = ['password', 'remember_token',];
 
     /**
      * Get the attributes that should be cast.
@@ -43,5 +36,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function emplyer(): HasOne
+    {
+        return $this->hasOne(Employer::class);
     }
 }
