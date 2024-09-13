dea94a0966e984f692409322fcc6c444abf656db
MazharSolkar <mazharsolkar65@gmail.com>
2024-09-13 19:47:58 +0530

    Employer: Adding Jobs Logic

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -16,4 +16,10 @@
             <span class="ml-2">{{ucfirst($option)}}</span>
         </label>
     @endforeach
+
+    @error($name)
+        <div class="mt-1 text-xs text-red-500">
+        {{ $message }}
+        </div>
+    @enderror
 </div>
